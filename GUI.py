c4871a10e9df19425c1ae723e84eb84b0b259185
Jiminy Haynes <jiminy@jjhaynes.co.uk>
2015-10-26 10:30:14 +0000

    draws gamewon senarios
    flashing boxes, still need to add menus and stuff

--- GUI.py
+++ GUI.py
@@ -3,11 +3,9 @@ screen=pygame.display.set_mode((610, 610))
 clock = pygame.time.Clock()
 board=pygame.image.load("board.png")
 
-
 def main():
     x=pygame.image.load('x.png')
     o=pygame.image.load('o.png')
-
     screen.blit(board,(0,0))
     used=[7,8,9,4,5,6,1,2,3]
     pygame.display.flip()
@@ -15,6 +13,7 @@ def main():
     count=0
     isgamewon=(False,9)
     while count<9 and not isgamewon[0]:
+        #every other time this ramdomises
         ev = pygame.event.get()
         for event in ev:
             if event.type == pygame.MOUSEBUTTONUP:
@@ -24,24 +23,21 @@ def main():
                     drawbox(o,x,turn,pos,used)
                     isgamewon=gamewon(used)
                     count+=1
-                    print(isgamewon)
                     if isgamewon[0]:
-                        print(isgamewon)
-                        drawline(wincombo)
+                        drawline(isgamewon[1],turn)
                         break
                     turn=not turn
                 else:
                     pass
                     print("no")
                     
-    if isgamewon[1]:
+    if count!=9:
         if turn: winner="1"
         else: winner="2"
         print("The Winner is Player {0}!".format(winner))
-    else: print("It's ok it's the taking part that counts!")
-    
+    else: print("No One Won!") 
     time.sleep(2)
-    pygame.quit()
+    pygame.quit()   
 
 def gamewon(used):#fix
     if used[0]==used[1] and used[1]==used[2]: return (True,1)
@@ -52,7 +48,7 @@ def gamewon(used):#fix
     elif used[2]==used[5] and used[5]==used[8]: return (True,6)
     elif used[0]==used[4] and used[4]==used[8]: return (True,7)
     elif used[2]==used[4] and used[4]==used[6]: return (True,8)
-    else: return (False,9)
+    else: return (False,999)
     
     
 def validmove(pos,used):
@@ -156,6 +152,81 @@ def drawbox(o,x,turn,pos,used):
     pygame.display.flip()
     return used
 
+def drawline(wincombo,turn):
+    xw=pygame.image.load("xwon.png")
+    ow=pygame.image.load("owin.png")
+    x=pygame.image.load("x.png")
+    o=pygame.image.load("o.png")
+    #creates the flashing effect
+    for count in range(0,6):
+        #changes every other time
+        if count/2==count//2:
+            if wincombo==1:
+                if turn:screen.blit(xw,(10,10)),screen.blit(xw,(210,10)),screen.blit(xw,(410,10))
+                else:screen.blit(ow,(10,10)),screen.blit(ow,(210,10)),screen.blit(ow,(410,10))
+                    
+            elif wincombo==2:
+                if turn:screen.blit(xw,(10,210)),screen.blit(xw,(210,210)),screen.blit(xw,(410,210))
+                else:screen.blit(ow,(10,210)),screen.blit(ow,(210,210)),screen.blit(ow,(410,210))
+               
+            elif wincombo==3:
+                if turn:screen.blit(xw,(10,410)),screen.blit(xw,(210,410)),screen.blit(xw,(410,410))
+                else:screen.blit(ow,(10,410)),screen.blit(ow,(210,410)),screen.blit(ow,(410,410))
+                    
+            elif wincombo==4:
+                if turn:screen.blit(xw,(10,10)),screen.blit(xw,(10,210)),screen.blit(xw,(10,410))
+                else:screen.blit(ow,(10,10)),screen.blit(ow,(10,210)),screen.blit(ow,(10,410))
+                    
+            elif wincombo==5:
+                if turn:screen.blit(xw,(210,10)),screen.blit(xw,(210,210)),screen.blit(xw,(210,410))
+                else:screen.blit(ow,(210,10)),screen.blit(ow,(210,210)),screen.blit(ow,(210,410))
+                    
+            elif wincombo==6:
+                if turn:screen.blit(xw,(410,10)),screen.blit(xw,(410,210)),screen.blit(xw,(410,410))
+                else:screen.blit(ow,(410,10)),screen.blit(ow,(410,210)),screen.blit(ow,(410,410))
+                    
+            elif wincombo==7:
+                if turn:screen.blit(xw,(10,10)),screen.blit(xw,(210,210)),screen.blit(xw,(410,410))
+                else:screen.blit(ow,(10,10)),screen.blit(ow,(210,210)),screen.blit(ow,(410,410))
+                    
+            elif wincombo==8:
+                if turn:screen.blit(xw,(410,10)),screen.blit(xw,(210,210)),screen.blit(xw,(10,410))
+                else:screen.blit(ow,(410,10)),screen.blit(ow,(210,210)),screen.blit(ow,(10,410))
+                
+        else:
+            if wincombo==1:
+                if turn:screen.blit(x,(10,10)),screen.blit(x,(210,10)),screen.blit(x,(410,10))
+                else:screen.blit(o,(10,10)),screen.blit(o,(210,10)),screen.blit(o,(410,10))
+                    
+            elif wincombo==2:
+                if turn:screen.blit(x,(10,210)),screen.blit(x,(210,210)),screen.blit(x,(410,210))
+                else:screen.blit(o,(10,210)),screen.blit(o,(210,210)),screen.blit(o,(410,210))
+               
+            elif wincombo==3:
+                if turn:screen.blit(x,(10,410)),screen.blit(x,(210,410)),screen.blit(x,(410,410))
+                else:screen.blit(o,(10,410)),screen.blit(o,(210,410)),screen.blit(o,(410,410))
+                    
+            elif wincombo==4:
+                if turn:screen.blit(x,(10,10)),screen.blit(x,(10,210)),screen.blit(x,(10,410))
+                else:screen.blit(o,(10,10)),screen.blit(o,(10,210)),screen.blit(o,(10,410))
+                    
+            elif wincombo==5:
+                if turn:screen.blit(x,(210,10)),screen.blit(x,(210,210)),screen.blit(x,(210,410))
+                else:screen.blit(o,(210,10)),screen.blit(o,(210,210)),screen.blit(o,(210,410))
+                    
+            elif wincombo==6:
+                if turn:screen.blit(x,(410,10)),screen.blit(x,(410,210)),screen.blit(x,(410,410))
+                else:screen.blit(o,(410,10)),screen.blit(o,(410,210)),screen.blit(o,(410,410))
+                    
+            elif wincombo==7:
+                if turn:screen.blit(x,(10,10)),screen.blit(x,(210,210)),screen.blit(x,(410,410))
+                else:screen.blit(o,(10,10)),screen.blit(o,(210,210)),screen.blit(o,(410,410))
+                    
+            elif wincombo==8:
+                if turn:screen.blit(x,(410,10)),screen.blit(x,(210,210)),screen.blit(x,(10,410))
+                else:screen.blit(o,(410,10)),screen.blit(o,(210,210)),screen.blit(o,(10,410))
+        pygame.display.flip()
+        time.sleep(0.5)
 
 if __name__=="__main__":
     main()
